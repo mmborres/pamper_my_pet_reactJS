1be52764ee2bd3a8d24fa7276e4d49015ab6be76
Purvi <pandyapurvi134@yahoo.in>
2019-05-10 13:01:57 +1000

    started basic framework

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Product from './Product';
+import Item from './Item';
+
+function App() {
+  return (
+    <div className="App">
+      <Product />
+      <Item />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Item.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+
+class Item extends Component {
+  render () {
+    return (
+      <div>
+        <Details />
+      </div>
+    );
+  }
+};
+
+class Details extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      click : ''
+    }
+    this._handleClick = this._handleClick.bind(this);
+  }
+  _handleClick(event){
+    event.preventDefault();
+    this.setState({ click : this.state.click});
+    console.log('hi');
+  }
+
+   render(){
+
+
+     return(
+       <div>
+       <h2>Name: of the Item will go here</h2>
+       <img src="#"/>
+       <h4>Prize: of the item </h4>
+       <p>Descrption</p>
+       <button onClick={this._handleClick}>Add to Cart</button>
+       <button onClick={this._handleClick}>Buy Now</button>
+       </div>
+     );
+   }
+};
+export default Item;
