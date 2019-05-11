38684b2a96fc7ae55b98e61b81b65972e65170e6
Mabeth <mmborres@gmail.com>
2019-05-12 04:38:48 +1000

    Create Order.js

---
+++ src/components/Order.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Footer from './Footer.js'
+
+class Order extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Orders</h2>
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default Order;
