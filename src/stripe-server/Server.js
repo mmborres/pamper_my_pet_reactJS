9cf18fe9902854c3b02cbdb44fa51e8fda83bc50
Mabeth <mmborres@gmail.com>
2019-05-16 09:10:24 +1000

    Update Server.js

--- src/stripe-server/Server.js
+++ src/stripe-server/Server.js
@@ -4,6 +4,8 @@ const stripe = require("stripe")("sk_test_4eC39HqLyjWDarjtT1zdp7dc");
 app.use(require("body-parser").text());
 
 app.post("/charge", async (req, res) => {
+  console.log("SERVER req ");
+  console.log(req);
     try {
       let {status} = await stripe.charges.create({
         amount: 2000,
@@ -12,10 +14,20 @@ app.post("/charge", async (req, res) => {
         source: req.body
       });
   
+      console.log("Response STATUS");
+      console.log(status);
+      
       res.json({status});
+
+      console.log("JSON : " + res);
     } catch (err) {
       res.status(500).end();
     }
   });
 
 app.listen(3000, () => console.log("Listening on port 3000"));
+
+
+// RUN
+// npm install express body-parser stripe
+// node Server.js
